4d794258d459c09dd7f1b949476052a1048a46ed
Pranav Teegavarapu <pranavnt@outlook.com>
2024-04-21 16:08:54 -0700

    trained policygit add .

--- sac_ur5e/main.py
+++ sac_ur5e/main.py
@@ -1,11 +1,29 @@
 import gymnasium as gym
 from ur5e_env import ReachingUR5eEnv
-
 from stable_baselines3 import SAC
 
-env = gym.make("Pendulum-v1", render_mode="human")
-
-model = SAC("MlpPolicy", env, verbose=1)
-
+# Create the environment
 env = ReachingUR5eEnv(render=True)
 
+# Create the SAC model
+model = SAC("MlpPolicy", env, verbose=1)
+
+# Set the number of training timesteps
+total_timesteps = 100000
+
+# Training loop
+model.learn(total_timesteps=total_timesteps)
+
+# Save the trained model
+model.save("sac_reaching_ur5e")
+
+# Evaluate the trained model
+obs = env.reset()
+done = False
+while not done:
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+
+# Close the environment
+env.close()
--- sac_ur5e/ur5e_env.py
+++ sac_ur5e/ur5e_env.py
@@ -1,21 +1,32 @@
-import mujoco
+import gymnasium as gym
 import random
-import time
-import mujoco.viewer
+from gymnasium import spaces
+import numpy as np
+import mujoco
 from dm_control import mujoco as dm_mujoco
 from ur5e_ik import qpos_from_site_pose
 
-class ReachingUR5eEnv:
+class ReachingUR5eEnv(gym.Env):
     def __init__(self, render=False):
+        super().__init__()
         self.model = mujoco.MjModel.from_xml_path("./mujoco_menagerie/universal_robots_ur5e/scene.xml")
         self.data = mujoco.MjData(self.model)
         self.physics = dm_mujoco.Physics.from_xml_path("./mujoco_menagerie/universal_robots_ur5e/scene.xml")
-        self.render = render
-        if self.render:
-            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+        self.render_mode = render
 
-    def step(self):
-        new_pos = (random.random()*5, random.random()*5, random.random()*5)
+        # Define the observation space
+        obs_low = np.full(6, -np.inf)
+        obs_high = np.full(6, np.inf)
+        self.observation_space = spaces.Box(low=obs_low, high=obs_high, dtype=np.float32)
+
+        # Define the action space
+        action_low = np.array([-5, -5, -5])
+        action_high = np.array([5, 5, 5])
+        self.action_space = spaces.Box(low=action_low, high=action_high, dtype=np.float32)
+        self.target_pos = np.array([0.5, 0.5, 0.5])
+
+    def step(self, action):
+        new_pos = action
         ik_result = qpos_from_site_pose(
             self.physics,
             site_name="attachment_site",
@@ -26,16 +37,34 @@ class ReachingUR5eEnv:
         )
         self.data.qpos[:6] = ik_result.qpos[:6]
         mujoco.mj_step(self.model, self.data)
-        if self.render:
-            with self.viewer.lock():
-                self.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(self.data.time % 2)
-                self.viewer.sync()
 
-    def reset(self):
+        observation = self.data.qpos[:6]
+        dist_to_target = np.linalg.norm(observation[:3] - self.target_pos)
+        reward = -dist_to_target
+        if reward > -0.5:
+            done = True
+            print("DONEEE")
+            self.reset()
+            self.target_pos = np.array([random.random(), random.random(), random.random()])
+        else:
+            done = False
+        info = {}
+        truncated = False
+
+        print(reward)
+
+        return observation, reward, done, truncated, info
+
+    def reset(self, seed=None):
+        super().reset(seed=seed)
         mujoco.mj_resetData(self.model, self.data)
-        if self.render:
-            self.viewer.reset()
+        observation = self.data.qpos[:6]
+        return observation, {}
+
+    def render(self):
+        if self.render_mode:
+            mujoco.mj_forward(self.model, self.data)
+            mujoco.mjr_render(self.model, self.data)
 
     def close(self):
-        if self.render:
-            self.viewer.close()
+        pass
