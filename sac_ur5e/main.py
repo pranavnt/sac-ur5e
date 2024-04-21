da753b7304b412b3134bbd6968e48907c687fc80
Pranav Teegavarapu <pranavnt@outlook.com>
2024-04-21 15:15:35 -0700

    ik

--- sac_ur5e/main.py
+++ sac_ur5e/main.py
@@ -1,23 +1,11 @@
 import gymnasium as gym
+from ur5e_env import ReachingUR5eEnv
 
 from stable_baselines3 import SAC
 
 env = gym.make("Pendulum-v1", render_mode="human")
 
 model = SAC("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
-model.save("sac_pendulum")
 
-del model # remove to demonstrate saving and loading
-
-model = SAC.load("sac_pendulum")
-
-obs, info = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
-    
-    print(obs, reward, terminated, truncated, info)
+env = ReachingUR5eEnv(render=True)
 
--- sac_ur5e/ur5e_env.py
+++ sac_ur5e/ur5e_env.py
@@ -1,2 +1,41 @@
 import mujoco
+import random
+import time
 import mujoco.viewer
+from dm_control import mujoco as dm_mujoco
+from ur5e_ik import qpos_from_site_pose
+
+class ReachingUR5eEnv:
+    def __init__(self, render=False):
+        self.model = mujoco.MjModel.from_xml_path("./mujoco_menagerie/universal_robots_ur5e/scene.xml")
+        self.data = mujoco.MjData(self.model)
+        self.physics = dm_mujoco.Physics.from_xml_path("./mujoco_menagerie/universal_robots_ur5e/scene.xml")
+        self.render = render
+        if self.render:
+            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+
+    def step(self):
+        new_pos = (random.random()*5, random.random()*5, random.random()*5)
+        ik_result = qpos_from_site_pose(
+            self.physics,
+            site_name="attachment_site",
+            target_pos=new_pos,
+            joint_names=["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint",
+                         "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"],
+            max_steps=100
+        )
+        self.data.qpos[:6] = ik_result.qpos[:6]
+        mujoco.mj_step(self.model, self.data)
+        if self.render:
+            with self.viewer.lock():
+                self.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(self.data.time % 2)
+                self.viewer.sync()
+
+    def reset(self):
+        mujoco.mj_resetData(self.model, self.data)
+        if self.render:
+            self.viewer.reset()
+
+    def close(self):
+        if self.render:
+            self.viewer.close()
